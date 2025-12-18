fdde388db14e854c6d488aa07786caf6810468d9
ccn <ccn@cuppajoeman.com>
2025-12-18 15:31:38 -0500

    feat: initial logic

---
+++ collision_utils.hpp
@@ -0,0 +1,25 @@
+#ifndef COLLISION_UTILS_HPP
+#define COLLISION_UTILS_HPP
+
+#include "sbpt_generated_includes.hpp"
+
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace collision_utils {
+
+enum class Symbol { a, b };
+
+struct CollisionManifold {
+    Symbol object_that_normal_exists_on;
+    glm::vec3 normal_generating_least_overlap;
+    float penetration;
+    std::vector<glm::vec3> contact_points; // optional for now
+};
+
+CollisionManifold detect_convex_collision(const draw_info::IndexedVertexPositions &a, const glm::vec3 &posA,
+                                          const draw_info::IndexedVertexPositions &b, const glm::vec3 &posB);
+
+} // namespace collision_utils
+
+#endif // COLLISION_UTILS_HPP
